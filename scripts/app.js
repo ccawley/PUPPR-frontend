dc37a4b10e300a123b4dcca3b9c81fded5419b60
Curtis Cawley <curtis.cawley@icloud.com>
2018-02-13 15:57:42 -0800

    dogs page is rendering dog profiles in a basic format

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
-  let url = "http://localhost:3000/dogs"
-  let button = document.querySelector("#axios")
+  // let button = document.querySelector("button")
+  axios.get("http://localhost:3000/dogs")
+    .then(function(result) {
+      renderDogs(result.data)
+    })
+    .catch(function() {
+      alert("ERROR!")
+    })
 
-  button.addEventListener("click", function() {
-    axios.get(url)
-      .then(function(res) {
-        let name = res.data[0].name
-        console.log(name)
-        document.querySelector("li").innerHTML = name
-      })
-      .catch(function() {
-        alert("ERROR!")
-      })
-  })
+  function renderDogs(data) {
+    console.log(data)
+    for(let i = 0; i < data.length; i++) {
+      let dogCard = document.createElement("div")
+      dogCard.style.background = `url(${data[i].picture_url})`
+      $(dogCard).addClass("dogCard")
+      dogCard.innerHTML = `<div>${data[i].name}</div>`
+      let dogCards = document.querySelector("#dog-cards")
+      dogCards.appendChild(dogCard)
+    }
+  }
 
 })
