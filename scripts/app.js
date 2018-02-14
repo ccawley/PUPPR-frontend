c73446edcab2552154f958b151013ad21507fb3a
Curtis Cawley <curtis.cawley@icloud.com>
2018-02-14 12:55:39 -0800

    added code for initializing materialize javascript

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,24 @@
-document.addEventListener('DOMContentLoaded', function(){
-  // let button = document.querySelector("button")
-  axios.get("http://localhost:3000/dogs")
-    .then(function(result) {
-      renderDogs(result.data)
-    })
-    .catch(function() {
-      alert("ERROR!")
-    })
+$(document).ready(function(){
+  $('select').select();
+});
 
-  function renderDogs(data) {
-    console.log(data)
-    for(let i = 0; i < data.length; i++) {
-      let dogCard = document.createElement("div")
-      dogCard.style.background = `url(${data[i].picture_url})`
-      $(dogCard).addClass("dogCard")
-      dogCard.innerHTML = `<div>${data[i].name}</div>`
-      let dogCards = document.querySelector("#dog-cards")
-      dogCards.appendChild(dogCard)
-    }
+// let button = document.querySelector("button")
+axios.get("http://localhost:3000/dogs")
+  .then(function(result) {
+    renderDogs(result.data)
+  })
+  .catch(function() {
+    alert("ERROR!")
+  })
+
+function renderDogs(data) {
+  console.log(data)
+  for(let i = 0; i < data.length; i++) {
+    let dogCard = document.createElement("div")
+    dogCard.style.background = `url(${data[i].picture_url})`
+    $(dogCard).addClass("dogCard")
+    dogCard.innerHTML = `<div>${data[i].name}</div>`
+    let dogCards = document.querySelector("#dog-cards")
+    dogCards.appendChild(dogCard)
   }
-
-})
+}
