5f4a4f2f28a4f0bee323024aabf5f27760f141c2
Curtis Cawley <curtis.cawley@icloud.com>
2018-02-15 13:31:24 -0800

    changed file from html to js, oops

---
+++ scripts/dog-profile.js
@@ -0,0 +1,19 @@
+axios.get("http://localhost:3000/dogs")
+  .then(function(result) {
+    renderDogs(result.data)
+  })
+  .catch(function() {
+    alert("ERROR!")
+  })
+
+function renderDogs(data) {
+  console.log(data)
+  for(let i = 0; i < data.length; i++) {
+    let dogCard = document.createElement("div")
+    dogCard.style.background = `url(${data[i].picture_url})`
+    $(dogCard).addClass("dogCard")
+    dogCard.innerHTML = `<div>${data[i].name}</div>`
+    let dogCards = document.querySelector("#dog-cards")
+    dogCards.appendChild(dogCard)
+  }
+}
