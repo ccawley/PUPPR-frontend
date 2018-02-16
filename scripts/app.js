3ccd28c13ea12b7ebe92f31faa95a866d7e5c401
Curtis Cawley <curtis.cawley@icloud.com>
2018-02-15 16:30:10 -0800

    can now click on a dog and it goes to it's profile page

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,41 @@ axios.get("http://localhost:3000/dogs")
     alert("ERROR!")
   })
 
+function getDog(id) {
+  return axios.get("http://localhost:3000/dogs/" + id)
+}
+
+let dogCards = document.querySelector('#dog-cards')
+let dogDetails = document.querySelector('#dog-details')
+
+dogCards.addEventListener('click', renderDogDetail)
+
+function renderDogDetail(e) {
+  dogCards.style.display = 'none'
+  let id = e.target.getAttribute('data-id')
+  getDog(id)
+  .then(function(result) {
+    console.log(result.data)
+    let details = document.createElement('div')
+    details.innerHTML = result.data.name
+    let dogDetails = document.querySelector("#dog-details")
+    dogDetails.appendChild(details)
+  })
+  .catch(function() {
+    alert("ERROR!")
+  })
+}
+
 function renderDogs(data) {
   console.log(data)
   for(let i = 0; i < data.length; i++) {
     let dogCard = document.createElement("div")
     dogCard.style.background = `url(${data[i].picture_url})`
+    dogCard.setAttribute('data-id', `${data[i].id}`)
     $(dogCard).addClass("dogCard")
     dogCard.innerHTML = `<div>${data[i].name}</div>`
     let dogCards = document.querySelector("#dog-cards")
     dogCards.appendChild(dogCard)
   }
+
 }
--- scripts/dog-profile.js
+++ scripts/dog-profile.js
@@ -1,11 +1,1 @@
-axios.get("http://localhost:3000/dogs/id")
-  .then(function(result) {
-    renderDog(result.data)
-  })
-  .catch(function() {
-    alert("ERROR!")
-  })
 
-function renderDog(data) {
-  console.log(data)
-}
