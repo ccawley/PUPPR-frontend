a62175f1b84e7f123409086d1dec128ff63c974b
Curtis Cawley <curtis.cawley@icloud.com>
2018-02-19 14:51:39 -0600

    added different register buttons for owners and petters and sign in buttons for both as well

--- scripts/landing-page.js
+++ scripts/landing-page.js
@@ -1,5 +1,20 @@
-let signUpButton = document.querySelector("#sign-up")
+let ownerSignUp = document.querySelector("#sign-up-owner")
+let petterSignUp = document.querySelector("#sign-up-petter")
+let ownerSignIn = document.querySelector("#sign-in-owner")
+let petterSignIn = document.querySelector("#sign-in-petter")
 
-signUpButton.addEventListener("click", function() {
+ownerSignUp.addEventListener("click", function() {
   window.location.replace("/create-owner.html")
+})
+
+petterSignUp.addEventListener("click", function() {
+  window.location.replace("/create-petter.html")
+})
+
+ownerSignIn.addEventListener("click", function() {
+  window.location.replace("/owner-sign-in.html")
+})
+
+petterSignIn.addEventListener("click", function() {
+  window.location.replace("/petter-sign-in.html")
 })
