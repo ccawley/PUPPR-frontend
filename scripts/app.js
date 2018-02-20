9ea0d986b02f16cee4fef56633e2001913fbd040
Curtis Cawley <curtis.cawley@icloud.com>
2018-02-19 23:31:06 -0600

    rearranged code and added explanatory comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+// Retrieves all dogs info from the database & calls the render function below.
 axios.get("http://localhost:3000/dogs")
   .then(function(result) {
     renderDogs(result.data)
@@ -6,31 +7,10 @@ axios.get("http://localhost:3000/dogs")
     alert("ERROR!")
   })
 
-function getDog(id) {
-  return axios.get("http://localhost:3000/dogs/" + id)
-}
-
 let dogCards = document.querySelector('#dog-cards')
 let dogDetails = document.querySelector('#dog-details')
 
-dogCards.addEventListener('click', renderDogDetail)
-
-function renderDogDetail(e) {
-  dogCards.style.display = 'none'
-  let id = e.target.getAttribute('data-id')
-  getDog(id)
-  .then(function(result) {
-    console.log(result.data)
-    let details = document.createElement('div')
-    details.innerHTML = result.data.name
-    let dogDetails = document.querySelector("#dog-details")
-    dogDetails.appendChild(details)
-  })
-  .catch(function() {
-    alert("ERROR!")
-  })
-}
-
+// Renders the page with a "mini-profile" for each dog in the database.
 function renderDogs(data) {
   console.log(data)
   for(let i = 0; i < data.length; i++) {
@@ -49,4 +29,29 @@ function renderDogs(data) {
     dogCards.appendChild(dogCard)
   }
 
+}
+
+// Retrieves a specific dogs info from the server database.
+function getDog(id) {
+  return axios.get("http://localhost:3000/dogs/" + id)
+}
+
+// When a "mini-profile" is clicked, the full profile for the dog is displayed.
+dogCards.addEventListener('click', renderDogDetail)
+
+// Renders the selected dog's profile to the page.
+function renderDogDetail(e) {
+  dogCards.style.display = 'none'
+  let id = e.target.getAttribute('data-id')
+  getDog(id)
+  .then(function(result) {
+    console.log(result.data)
+    let details = document.createElement('div')
+    details.innerHTML = result.data.name
+    let dogDetails = document.querySelector("#dog-details")
+    dogDetails.appendChild(details)
+  })
+  .catch(function() {
+    alert("ERROR!")
+  })
 }
